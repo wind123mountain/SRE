3fac02d294873abfbf1894618d9c07893d6840c8
Đào Quốc Phong <57493825+wind123mountain@users.noreply.github.com>
2026-05-07 16:03:19 +0700

    Increase temperature parameter from 2.0 to 3.0

--- scripts/qwen1_5_gpt2_340m.sh
+++ scripts/qwen1_5_gpt2_340m.sh
@@ -5,7 +5,7 @@ SEED=$1
 # ==== Định nghĩa các biến ====
 BASE_PATH=.
 HARD_LABEL_LOSS_WEIGHT=0.5
-TEMPERATURE=2.0
+TEMPERATURE=3.0
 LR=1e-4
 OUTPUT_DIR="${BASE_PATH}/outputs/qwen1_5_gpt2_340m/seed-${SEED}-hlw-${HARD_LABEL_LOSS_WEIGHT}-temp-${TEMPERATURE}-lr-${LR}"
 CKPT_NAME="qwen1_5_gpt2_340m-checkpoint"
