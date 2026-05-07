6a9b70c13a0cb4fb2016cc7d56a24b359269de50
Sonnguyen4002 <nguyenhoangson041104@gmail.com>
2026-05-07 10:09:06 +0700

    new

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,31 +2,29 @@
 
 SEED=$1
 
-# ==== Định nghĩa các biến ====
 BASE_PATH=.
-OUTPUT_DIR="${BASE_PATH}/outputs/test/seed-${SEED}"
-CKPT_NAME="test-checkpoint"
+OUTPUT_DIR="${BASE_PATH}/outputs/opt/seed-${SEED}"
+CKPT_NAME="qwen-2-5-opt-checkpoint"
 
 mkdir -p ${OUTPUT_DIR}
 
-# Gom tham số vào OPTS
 OPTS=""
 # data
-OPTS+=" --train_data ${BASE_PATH}/llm_data/dolly/train.jsonl"
-OPTS+=" --val_data ${BASE_PATH}/llm_data/dolly/valid.jsonl"
-OPTS+=" --test_data ${BASE_PATH}/llm_data/vicuna/valid.jsonl"
+OPTS+=" --train_data ${BASE_PATH}/data/dolly/train.jsonl"
+OPTS+=" --val_data ${BASE_PATH}/data/dolly/valid.jsonl"
+OPTS+=" --test_data ${BASE_PATH}/data/vicuna/valid.jsonl"
 
 # training
 OPTS+=" --num_train_epochs 15"
-OPTS+=" --batch_size 4"
+OPTS+=" --batch_size 16"
 OPTS+=" --val_batch_size 32"
 OPTS+=" --learning_rate 5e-4"
 OPTS+=" --max_len 320"
 OPTS+=" --pad_to_multiple_of 1"
 
 # devices
-OPTS+=" --teach_device cuda:1"
-OPTS+=" --student_device cuda:0"
+OPTS+=" --teach_device cuda:2"
+OPTS+=" --student_device cuda:2"
 
 # loss
 OPTS+=" --hard_label_loss_weight 0.5"
@@ -37,29 +35,29 @@ OPTS+=" --span_weight_pooling True"
 OPTS+=" --span_loss_weight True"
 OPTS+=" --p 1.0"
 
-OPTS+=" --n_encoder_finetuned 12"
+OPTS+=" --n_encoder_finetuned 32"
 OPTS+=" --hidden_loss_weights 1"
 
 OPTS+=" --entropy_weight True"
-OPTS+=" --student_layer_mapping 11 12"
-OPTS+=" --teacher_layer_mapping 23 24"
+OPTS+=" --student_layer_mapping 28 32"
+OPTS+=" --teacher_layer_mapping 25 28"
 OPTS+=" --split_layer_mapping 0 1 2"
 OPTS+=" --w_span_loss 2.0"
 
 # models
-OPTS+=" --teacher_embedding_dimension 2048"
+OPTS+=" --teacher_embedding_dimension 3584"
 OPTS+=" --output_dir ${OUTPUT_DIR}"
-OPTS+=" --teacher_model Qwen/Qwen1.5-1.8B"
-OPTS+=" --teacher_tokenizer Qwen/Qwen1.5-1.8B"
-OPTS+=" --student_model openai-community/gpt2"
-OPTS+=" --student_tokenizer openai-community/gpt2"
+OPTS+=" --teacher_model VoCuc/Qwen2.5-7B-Instruct-Dolly-SFT"
+OPTS+=" --teacher_tokenizer Qwen/Qwen2.5-7B-Instruct"
+OPTS+=" --student_model facebook/opt-2.7b"
+OPTS+=" --student_tokenizer facebook/opt-2.7b"
 
 # hf token
 # OPTS+=" --hf_token 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"
 
 # extra arguments
 OPTS+=" --seed ${SEED}"
-OPTS+=" --student_model_type gpt2"
+OPTS+=" --student_model_type opt"
 OPTS+=" --teacher_model_type qwen"
 OPTS+=" --use_lora True"
 OPTS+=" --grad_accum_steps 4"
