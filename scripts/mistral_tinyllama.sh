529c701b2a861e17aff198905039167b22de8042
Đào Quốc Phong <57493825+wind123mountain@users.noreply.github.com>
2026-05-10 13:41:27 +0700

    Update mistral_tinyllama.sh

--- scripts/mistral_tinyllama.sh
+++ scripts/mistral_tinyllama.sh
@@ -4,7 +4,7 @@ SEED=$1
 
 # ==== Định nghĩa các biến ====
 BASE_PATH=.
-HARD_LABEL_LOSS_WEIGHT=0.3
+HARD_LABEL_LOSS_WEIGHT=0.5
 TEMPERATURE=5.0
 LR=1e-4
 OUTPUT_DIR="${BASE_PATH}/outputs/tiny_llama/seed-${SEED}-hlw-${HARD_LABEL_LOSS_WEIGHT}-temp-${TEMPERATURE}-lr-${LR}"
